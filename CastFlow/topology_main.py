0f250b6989b22579548581833e1aea227db1a093
Caio Viel <caioviel@gmail.com>
2011-10-23 18:27:55 -0200

    Arquivos principais renomeados.

---
+++ CastFlow/topology_main.py
@@ -0,0 +1,22 @@
+'''
+Created on Oct 20, 2011
+
+@author: caioviel
+'''
+
+import sys
+from topologyserver.TopologyServer import TopologyServer
+
+if __name__ == '__main__':
+    fileName = 'brite1.brite'
+    
+    if (len(sys.argv) > 1):
+        fileName = sys.argv[1]
+        
+    server = TopologyServer('brite1.brite')
+    server.startListen()
+        
+        
+        
+        
+    
