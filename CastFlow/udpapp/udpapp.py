a75d759337aa66041cee7a984f354d85b94c106b
Caio Viel <caioviel@gmail.com>
2011-11-22 10:37:26 -0200

    Refazendo a coleta de dados do udpapp

--- CastFlow/udpapp/udpapp.py
+++ CastFlow/udpapp/udpapp.py
@@ -45,7 +45,7 @@ class DataCollector(object):
             
     def collect(self, str_data):
         self.file.write(str_data + "\n")
-        #self.file.flush()
+        self.file.flush()
 
 class UdpAppCollector(DataCollector):
     def __init__(self, prename ='udpapp', format='csv'):
@@ -210,9 +210,10 @@ elif sys.argv[1] == "-c":
 
     local_timestamp = repr( time.time() )
     source_id, packet_number, server_timestamp = parse_packet(data)
-    print 'First Packet; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
-    dc.write_header()
-    dc.collect_first_package(source_id, packet_number, server_timestamp, local_timestamp)
+    str_output = ""
+    str_output += 'First Packet; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
+    #dc.write_header()
+    #dc.collect_first_package(source_id, packet_number, server_timestamp, local_timestamp)
 
     sock.settimeout(1) #timeout setted to 1s.
 
@@ -221,6 +222,8 @@ elif sys.argv[1] == "-c":
     last_packet_number = long(packet_number)
     last_server_timestamp = float(server_timestamp)
     last_local_timestamp = float(local_timestamp)
+    
+    interrupted_count = 0
 
     while True:
         try:
@@ -229,17 +232,17 @@ elif sys.argv[1] == "-c":
             source_id, packet_number, server_timestamp = parse_packet(data)
             if interrupted_flow:
                 interrupted_flow = False
-                #print 'Resumed Flow; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
-                dc.collect_resumed_flow(source_id, packet_number, server_timestamp, local_timestamp)
+                str_output += 'Resumed Flow; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
+                #dc.collect_resumed_flow(source_id, packet_number, server_timestamp, local_timestamp)
             
             #print source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
             if current_source_id != source_id:
-                dc.collect_source_changed(source_id, packet_number, server_timestamp, local_timestamp)
-                #print 'Source Changed; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
+                #dc.collect_source_changed(source_id, packet_number, server_timestamp, local_timestamp)
+                str_output += 'Source Changed; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
             elif last_packet_number - long(packet_number) > 1:
-                dc.collect_package_lost(source_id, str(last_packet_number), 
+                #dc.collect_package_lost(source_id, str(last_packet_number), 
                                         packet_number, server_timestamp, local_timestamp)
-                #print 'Packet Lost; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
+                str_output += 'Packet Lost; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
             
             # Save the informations
             current_source_id = source_id
@@ -250,8 +253,14 @@ elif sys.argv[1] == "-c":
         except socket.timeout:
             if not interrupted_flow:
                 interrupted_flow = True
-                dc.collect_interrupted_flow(source_id, packet_number, server_timestamp, local_timestamp)
-                #print 'Interrupted Flow; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
+                interrupted_count = 0
+                #dc.collect_interrupted_flow(source_id, packet_number, server_timestamp, local_timestamp)
+                str_output += 'Interrupted Flow; ' + source_id + '; ' + packet_number + '; ' + server_timestamp + '; ' + local_timestamp
+            else:
+                interrupted_count += 1
+                if interrupted_count == 10:
+                    dc.collect(str_output)
+                    str_output = ""
 
         
 elif sys.argv[1] == "-m":
